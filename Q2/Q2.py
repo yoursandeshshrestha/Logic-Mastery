b630cc21951b4afa83ee038dddac5770d32c28c9
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-09-15 11:11:35 +0530

    feat: Add Python solution for 'Find the largest of three numbers'

---
+++ Q2/Q2.py
@@ -0,0 +1,7 @@
+# Find the largest of three numbers.
+
+def LargestNumber(Num1, Num2, Num3):
+    return max(Num1, Num2, Num3)
+
+largest = LargestNumber(10, 20, 30)
+print(f"The largest number is {largest}")
