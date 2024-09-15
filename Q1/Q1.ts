ab514a8c46e8f538ad66de3f9bff5702a77ae133
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-09-15 10:12:39 +0530

    feat: Add TypeScript solution for " Check if a number is even or odd\

---
+++ Q1/Q1.ts
@@ -0,0 +1,11 @@
+// Write a function to check if a number is even or odd.
+
+function CheckEvenorOdd(number: number) {
+  if (number % 2 === 0) {
+    console.log(`${number} is Even`);
+  } else {
+    console.log(`${number} is Odd`);
+  }
+}
+
+CheckEvenorOdd(5);
