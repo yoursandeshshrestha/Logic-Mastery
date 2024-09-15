f19f0f76acd6591e26016e48961c19f898ca43ec
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-09-15 10:13:23 +0530

    feat: Add Python solution for " Check if a number is even or odd\

---
+++ Q1/Q1.py
@@ -0,0 +1,10 @@
+# Write a function to check if a number is even or odd.
+
+def CheckEvenorOdd(Number):
+    if Number % 2 == 0:
+        print(f"{Number} is even")
+    else:
+        print(f"{Number} is odd")
+    
+
+CheckEvenorOdd(4)
