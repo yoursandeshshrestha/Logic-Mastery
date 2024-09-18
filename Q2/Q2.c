f84f686f62e4e2841644b0ce3c1c5a31b33dae3c
GungunRaniSahu <155472014+GungunRaniSahu@users.noreply.github.com>
2024-09-18 14:42:38 +0530

    Create Q2.c

---
+++ Q2/Q2.c
@@ -0,0 +1,12 @@
+/* Find the largest of three numbers*/
+#include<stdio.h>
+void main()
+{
+  int a , b, c , largest;
+
+  printf("Enter the first number\n");
+  scanf("%d" , &a);
+
+  printf("Enter the second number\n");
+  scanf("%d");
+}
