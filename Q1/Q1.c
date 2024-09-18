7d581aad21f8d7fdca97550be49930171d061fa3
GungunRaniSahu <155472014+GungunRaniSahu@users.noreply.github.com>
2024-09-18 14:33:25 +0530

    Q1.c

--- Q1/Q1.c
+++ Q1/Q1.c
@@ -1,1 +1,18 @@
 //# Write a function to check if a number is even or odd.
+#include<stdio.h>
+void main()
+{
+  int num;
+  printf("Enter a number of your choice: \n");
+  scanf("%d" , &num);
+
+  if(num%2==0)
+  {
+    printf("You have entered an even number\n");
+  }
+
+  else
+  {
+    printf("You have entered an odd number\n");
+  }
+}
