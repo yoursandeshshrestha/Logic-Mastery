13f0e1133b51b268b82d4e86920760abb077dacd
Sandesh Shrestha <156994003+yoursandeshshrestha@users.noreply.github.com>
2024-09-22 13:19:47 +0530

    feat: Fixed error and completed the code

--- Q2/Q2.c
+++ Q2/Q2.c
@@ -8,5 +8,20 @@ void main()
   scanf("%d" , &a);
 
   printf("Enter the second number\n");
-  scanf("%d");
+  scanf("%d", &b);
+
+  printf("Enter the third number\n");
+  scanf("%d", &c);
+  
+  if (a >= b && a >= c) {
+    largest = a;
+  } 
+  else if (b >= a && b >= c) {
+    largest = b;
+  } 
+  else {
+    largest = c;
+  }
+
+  printf("The largest number is: %d\n", largest);
 }
