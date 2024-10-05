a4b83449e528f976f24f66dab8aecd0b8a3201ae
GungunRaniSahu <155472014+GungunRaniSahu@users.noreply.github.com>
2024-10-05 22:33:01 +0530

    Feat : C function to merge two sorted arrays

---
+++ Q15/Q15.c
@@ -0,0 +1,37 @@
+/*Merge two sorted arrays into one sorted array.*/
+#include <stdio.h>
+void main()
+{
+    int arr1[] = {1, 3, 5};
+    int arr2[] = {2, 4, 6};
+    int merged[6];
+    int i = 0, j = 0, k = 0;
+
+    while (i < 3 && j < 3) 
+    {
+        if (arr1[i] < arr2[j])
+        {
+            merged[k++] = arr1[i++];
+        } 
+        else
+        {
+            merged[k++] = arr2[j++];
+        }
+    }
+
+    while (i < 3)
+    {
+        merged[k++] = arr1[i++];
+    }
+
+    while (j < 3)
+    {
+        merged[k++] = arr2[j++];
+    }
+
+    for (i = 0; i < 6; i++)
+    {
+        printf("%d ", merged[i]);
+    }
+
+}
