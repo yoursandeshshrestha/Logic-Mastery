32c0650cc8c78acf1158c209540125f21d050083
GungunRaniSahu <155472014+GungunRaniSahu@users.noreply.github.com>
2024-10-06 22:44:01 +0530

    Feat: C++ program to count the character in the string

---
+++ Q18/Q18.cpp
@@ -0,0 +1,26 @@
+/*Count the character in the string*/
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    char mystring[100], ch;
+    int count = 0;
+
+    cout << "Enter a string: ";
+    cin >> mystring;  
+
+    cout << "Enter the character to count: ";
+    cin >> ch;  
+
+
+    for (int i = 0; i < strlen(mystring); i++) {
+        if (mystring[i] == ch) {
+            count++;
+        }
+    }
+
+    cout << "The character '" << ch << "' appears " << count << " times in the string." << endl;
+
+    return 0;
+}
