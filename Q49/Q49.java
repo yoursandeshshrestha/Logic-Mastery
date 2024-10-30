550a44ccced9c71b0ef72eca863783f47e977bdf
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-10-30 21:16:48 +0530

    Feat: Program to find the smallest missing positive integer in an array in typescript.

---
+++ Q49/Q49.java
@@ -0,0 +1,27 @@
+// Find the smallest missing positive integer in an array.
+
+public class Solution {
+    public static int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                int temp = nums[i];
+                nums[i] = nums[temp - 1];
+                nums[temp - 1] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        return n + 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(firstMissingPositive(new int[]{3, 4, -1, 1}));  // Output: 2
+    }
+}
