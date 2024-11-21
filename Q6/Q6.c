28ace9a5f070f8ecf8a30f9f50de7ff8d5c39fb8
Gungun Rani <155472014+GungunRaniSahu@users.noreply.github.com>
2024-11-21 23:53:27 +0530

    Feat: Add number inside an array

---
+++ Q6/Q6.c
@@ -0,0 +1,27 @@
+/*Add number inside an array*/
+include <stdio.h>
+void main()
+{
+    int n, i, sum = 0;
+    printf("Enter the size of an array : ");
+    scanf("%d", &n);
+
+    int arr[n]; 
+
+   
+    printf("Enter the elements of the array :\n");
+   
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+
+    for (i = 0; i < n; i++) 
+    {
+        sum += arr[i];
+    }
+
+ 
+    printf("Sum of the array elements : %d\n", sum);
+}
