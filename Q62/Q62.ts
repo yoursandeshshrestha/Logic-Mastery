b7b832213a3cc2aebbebb9f976599b6b7c7156b8
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-11-23 23:51:03 +0530

    Feat: Added Merge Sort in Typescript

---
+++ Q62/Q62.ts
@@ -0,0 +1,33 @@
+// Merge Sort
+
+function mergeSort(arr: number[]): number[] {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const mid = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0, mid));
+  const right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+}
+
+function merge(left: number[], right: number[]): number[] {
+  const result: number[] = [];
+  let i = 0,
+    j = 0;
+
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      result.push(left[i]);
+      i++;
+    } else {
+      result.push(right[j]);
+      j++;
+    }
+  }
+
+  return result.concat(left.slice(i)).concat(right.slice(j));
+}
+
+const array: number[] = [38, 27, 43, 3, 9, 82, 10];
+console.log("Sorted Array:", mergeSort(array));
