b14c3a3df63bd225405cd164e137e9605dae8619
Gungun Rani <155472014+GungunRaniSahu@users.noreply.github.com>
2025-01-09 05:33:12 +0530

    In-Order Traversal of a Binary Tree Without Recursion in c

---
+++ Q68/Q68.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+struct Node* createNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+void inOrderTraversal(struct Node* root) {
+    struct Node* stack[100]; 
+    int top = -1;      
+    struct Node* current = root;
+
+    while (current != NULL || top != -1) {
+        
+        while (current != NULL) {
+            stack[++top] = current;
+            current = current->left;
+        }
+
+        
+        current = stack[top--];
+        printf("%d ", current->data); 
+
+
+        current = current->right;
+    }
+}
+
+int main() {
+
+    struct Node* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+
+    printf("In-order traversal: ");
+    inOrderTraversal(root);
+
+    return 0;
+}
